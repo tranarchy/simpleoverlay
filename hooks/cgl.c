bb690bf7aaa4224efc0b2ec18e19b20304340bbf
tranarchy <150830424+tranarchy@users.noreply.github.com>
2025-09-11 18:59:23 +0200

    fixed issues with CGL

--- hooks/cgl.c
+++ hooks/cgl.c
@@ -1,26 +1,86 @@
-#include <stdbool.h>
+#include <dlfcn.h>
 
 #include <objc/runtime.h>
-#include <objc/message.h>
 
 #include "../include/glad.h"
 
 void draw_overlay(const char *interface, unsigned int *viewport);
 
+typedef int (*PFNCGLSETSURFACE)(void *gl, int cid, int wid, int sid);
+typedef int (*PFNCGLGETSURFACE)(void *gl, int *cid, int *wid, int *sid);
+typedef void* (*PFNCGLGETPIXELFORMAT)(void *gl);
+typedef int (*PFNCGLCREATECONTEXT)(void *pixel_format, void *share_context, void **gl);
+typedef void *(*PFNCGLGETCURRENTCONTEXT)(void);
+typedef int (*PFNCGLSETCURRENTCONTEXT)(void *gl);
+
+static PFNCGLSETSURFACE CGLSetSurface_ptr = NULL;
+static PFNCGLGETSURFACE CGLGetSurface_ptr = NULL;
+static PFNCGLGETPIXELFORMAT CGLGetPixelFormat_ptr = NULL;
+static PFNCGLCREATECONTEXT CGLCreateContext_ptr = NULL;
+static PFNCGLGETCURRENTCONTEXT CGLGetCurrentContext_ptr = NULL;
+static PFNCGLSETCURRENTCONTEXT CGLSetCurrentContext_ptr = NULL;
+
 static IMP flushBuffer_ptr;
 
-static bool glad_init = false;
+static int cid, wid, sid;
 
-static void flushBuffer(id self, SEL _cmd) {
-  if (!glad_init) {
-      gladLoadGL();
-      glad_init = true;
+static void *cgl_handle = NULL;
+static void *cgl_ctx, *prev_cgl_ctx = NULL;
+
+static const char *cgl_libs[] = { 
+  "/System/Library/Frameworks/OpenGL.framework/OpenGL", 
+  "/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL" 
+};
+
+static void *get_libcgl_addr(const char *proc_name) {
+  if (!cgl_handle) {
+    for (size_t i = 0; i < sizeof(cgl_libs) / sizeof(cgl_libs[0]); i++) {
+       cgl_handle = dlopen(cgl_libs[i], RTLD_LAZY);
+
+       if (cgl_handle) {
+          break;
+       }
+    } 
   }
 
+  return dlsym(cgl_handle, proc_name);
+}
+
+static void *get_cgl_ctx() {
+  if (cgl_ctx) {
+    return cgl_ctx;
+  }
+  
+  CGLGetPixelFormat_ptr = (PFNCGLGETPIXELFORMAT)get_libcgl_addr("CGLGetPixelFormat");
+  CGLCreateContext_ptr = (PFNCGLCREATECONTEXT)get_libcgl_addr("CGLCreateContext");
+
+  void *pixel_format = CGLGetPixelFormat_ptr(prev_cgl_ctx);
+
+  CGLCreateContext_ptr(pixel_format, 0, &cgl_ctx);
+
+  return cgl_ctx;
+}
+
+static void flushBuffer(id self, SEL _cmd) {
+  if (!CGLGetCurrentContext_ptr) {
+      gladLoadGL();
+      CGLGetCurrentContext_ptr = (PFNCGLGETCURRENTCONTEXT)get_libcgl_addr("CGLGetCurrentContext");
+      CGLSetCurrentContext_ptr = (PFNCGLSETCURRENTCONTEXT)get_libcgl_addr("CGLSetCurrentContext");
+      CGLGetSurface_ptr = (PFNCGLGETSURFACE)get_libcgl_addr("CGLGetSurface");
+      CGLSetSurface_ptr = (PFNCGLSETSURFACE)get_libcgl_addr("CGLSetSurface");
+      
+  }
   int viewport[4];
   glGetIntegerv(GL_VIEWPORT, viewport);
 
+  prev_cgl_ctx = CGLGetCurrentContext_ptr();
+  CGLGetSurface_ptr(prev_cgl_ctx, &cid, &wid, &sid);
+
+  CGLSetSurface_ptr(get_cgl_ctx(), cid, wid, sid);
+  CGLSetCurrentContext_ptr(get_cgl_ctx());
   draw_overlay("CGL", (unsigned int*)viewport);
+  glFlush();
+  CGLSetCurrentContext_ptr(prev_cgl_ctx);
 
   ((void(*)(id, SEL))flushBuffer_ptr)(self, _cmd);
 }
