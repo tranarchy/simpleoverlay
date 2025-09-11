302f725f87e0d00b7b1d037ee6767c7b166c322e
tranarchy <150830424+tranarchy@users.noreply.github.com>
2025-09-11 16:03:12 +0200

    changed how used mem is calculated on BSD and OSX

--- util/mem.c
+++ util/mem.c
@@ -5,13 +5,14 @@
 
 #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
   #include <unistd.h>
-  
   #include <sys/sysctl.h>
-  #include <sys/vmmeter.h>
 
-  #if defined(__FreeBSD__) || defined(__DragonFly__)
-      #include <vm/vm_param.h>
+  #if defined(__OpenBSD__)
+    #include <uvm/uvmexp.h>
+  #elif defined(__NetBSD__)
+    #include <uvm/uvm_extern.h>
   #endif
+  
 #elif defined(__APPLE__)
     #include <unistd.h>
     #include <mach/mach.h>
@@ -46,77 +47,85 @@
 
       fclose(fp);
 
-      overlay_info->mem = total - available;
-      overlay_info->mem /= (1024.0 * 1024.0);
+      overlay_info->mem = (total - available) / (1024.0 * 1024.0);
   }
-#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+#elif defined(__FreeBSD__) || defined(__DragonFly__)
+  static void get_mem_usage(s_overlay_info *overlay_info) {
+      int mib[4];
+      
+      size_t len, slen;
+
+      long long total;
+      int free, inactive, cache;
+
+      long long pagesize = sysconf(_SC_PAGESIZE);
+
+      mib[0] = CTL_HW;
+      mib[1] = HW_PHYSMEM;
+
+      len = sizeof(total);
+      sysctl(mib, 2, &total, &len, NULL, 0);
+
+      len = 4;
+      slen = sizeof(free);
+      sysctlnametomib("vm.stats.vm.v_free_count", mib, &len);
+      sysctl(mib, 4, &free, &slen, NULL, 0);
+
+      len = 4;
+      slen = sizeof(inactive);
+      sysctlnametomib("vm.stats.vm.v_inactive_count", mib, &len);
+      sysctl(mib, 4, &inactive, &slen, NULL, 0);
+
+      len = 4;
+      slen = sizeof(cache);
+      sysctlnametomib("vm.stats.vm.v_cache_count", mib, &len);
+      sysctl(mib, 4, &cache, &slen, NULL, 0);
+
+      long long used = (free + inactive + cache) * pagesize;
+      overlay_info->mem = (total - used) / (1024.0 * 1024.0 * 1024.0);
+  }
+#elif defined(__OpenBSD__) || defined(__NetBSD__)
   static void get_mem_usage(s_overlay_info *overlay_info) {
       int mib[2];
 
-      size_t len;
-      size_t slen;
-      long long total;
-      struct vmtotal vmtotal;
+      mib[0] = CTL_VM;
 
-      mib[0] = CTL_HW;
-
-      #if defined(__FreeBSD__) || defined(__DragonFly__)
-          mib[1] = HW_PHYSMEM;
+      #if defined(__OpenBSD__)
+        struct uvmexp uvmexp;
+        mib[1] = VM_UVMEXP;
       #else
-          mib[1] = HW_PHYSMEM64;
+        struct uvmexp_sysctl uvmexp;
+        mib[1] = VM_UVMEXP2;
       #endif
+      
+      long long pagesize = sysconf(_SC_PAGESIZE);
 
-      len = sizeof(total);
-      int ret = sysctl(mib, 2, &total, &len, NULL, 0);
+      size_t len = sizeof(uvmexp);
+
+      sysctl(mib, 2, &uvmexp, &len, NULL, 0);
+
+      long long used = (uvmexp.active + uvmexp.inactive + uvmexp.wired) * pagesize;
+      overlay_info->mem = used / (1024.0 * 1024.0 * 1024.0);
+  }
+#elif defined(__APPLE__)
+  static void get_mem_usage(s_overlay_info *overlay_info) {
+      vm_statistics64_data_t vm_stats; 
+      mach_msg_type_number_t slen = HOST_VM_INFO64_COUNT;
+
+      int ret = host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stats, &slen);
 
       if (ret == -1) {
-          return;
-      }
-
-      slen = sizeof(vmtotal);
-
-      #if defined(__FreeBSD__) || defined(__DragonFly__)
-          ret = sysctlbyname("vm.vmtotal", &vmtotal, &slen, NULL, 0);
-      #else
-          mib[0] = CTL_VM;
-          mib[1] = VM_METER;
-
-          ret = sysctl(mib, 2, &vmtotal, &slen, NULL, 0);
-      #endif
-
-      if (ret == -1) {
-          return;
+        return;
       }
 
       long long pagesize = sysconf(_SC_PAGESIZE);
 
-      #if defined(__OpenBSD__)
-          overlay_info->mem = (vmtotal.t_avm * pagesize) / (1024.0 * 1024.0 * 1024.0);
-      #else
-          long long free = vmtotal.t_free * pagesize;
-          overlay_info->mem = (total - free) / (1024.0 * 1024.0 * 1024.0);       
-      #endif
+      long long used = (vm_stats.active_count + vm_stats.inactive_count +
+              vm_stats.speculative_count + vm_stats.wire_count +
+              vm_stats.compressor_page_count - vm_stats.purgeable_count - vm_stats.external_page_count) * pagesize;
+
+      overlay_info->mem = used / (1024.0 * 1024.0 * 1024.0);
   }
-#elif defined(__APPLE__)
-    void get_mem_usage(s_overlay_info *overlay_info) {
-        vm_statistics64_data_t vm_stats; 
-        mach_msg_type_number_t slen = HOST_VM_INFO64_COUNT;
-
-        int ret = host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stats, &slen);
-
-        if (ret == -1) {
-            return;
-        }
-
-        long long pagesize = sysconf(_SC_PAGESIZE);
-
-        long long active = vm_stats.active_count * pagesize;
-        long long wire = vm_stats.wire_count * pagesize;
-        long long compression = vm_stats.compressor_page_count * pagesize;
-
-        overlay_info->mem = (active + wire + compression) / (1024.0 * 1024.0 * 1024.0);
-    }
-
 #endif
 
 void populate_mem(s_overlay_info *overlay_info) {
