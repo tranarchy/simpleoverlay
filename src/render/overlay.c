4b3c204af463fab135bbe99cacfeb85b928dbfe8
tranarchy <150830424+tranarchy@users.noreply.github.com>
2025-09-16 02:04:30 +0200

    added frametime graph

--- src/render/overlay.c
+++ src/render/overlay.c
@@ -1,5 +1,6 @@
 #define _XOPEN_SOURCE 500
 
+#include <math.h>
 #include <time.h>
 #include <stdio.h>
 #include <stddef.h>
@@ -57,6 +58,7 @@ static int frames;
 static long long prev_time;
 static long long prev_time_frametime;
 
+static float frametimes[100] = { 0 };
 static char vendor[128];
 
 static mu_Context *ctx = NULL;
@@ -72,11 +74,19 @@ static long long get_time_nano(void) {
 static void populate_overlay_info(void) {
     long long cur_time = get_time_nano();
 
+    float frametime = (cur_time - prev_time_frametime) / 1e6f;
+
+    for (int i = 0; i < 99; i++) {
+        frametimes[i] = frametimes[i + 1];
+    }
+
+    frametimes[99] = frametime;
+
     frames++;
 
     if (cur_time - prev_time >= 1e9) {
       overlay_info.fps = frames;
-      overlay_info.frametime = (cur_time - prev_time_frametime) / 1e6f;
+      overlay_info.frametime = frametime;
 
       if (!config.fps_only) {
         populate_cpu(&overlay_info);
@@ -212,13 +222,80 @@ void draw_overlay(const char *interface, unsigned int *viewport) {
       add_text(ctx, &init_rect, "RAM", " %.2f GiB", overlay_info.mem);
     }
 
-    win->rect.h += 5;
+    win->rect.h += 35;
     
+    mu_Rect frametime_r_base = mu_layout_next(ctx);
+    frametime_r_base.y += 15;
+    frametime_r_base.w =  win->rect.w - 5;
+    frametime_r_base.h = 1;
+
+    int graph_max_height = 10;
+    
+    float frametime_sum = 0;
+    float frametime_avg = 0;
+
+    for (int i = 1; i < 100; i++) {
+        float frametime = frametimes[i];
+
+        if (frametime == 0) {
+          break;
+        }
+
+        frametime_sum += frametime;
+    }
+
+    frametime_avg = frametime_sum / 100;
+
+    int frametime_element_width = (int)round((float)win->rect.w / 100.0f);
+  
+    for (int i = 0; i < 100; i++) {
+        float frametime = frametimes[i];
+
+        if (frametime == 0) {
+          break;
+        }
+
+        int percent = (frametime_avg / frametime) * 100;
+
+        if (percent >= 99 && percent <= 101) {
+            continue;
+        }
+
+        percent -= 100;
+
+        if (percent < -50) {
+          percent = -50;
+        } else if (percent > 50) {
+          percent = 50;
+        }
+
+        percent /= graph_max_height;
+
+        mu_Rect frametime_r = mu_rect(0, 0, 0, 0);
+        frametime_r.x = frametime_r_base.x + (i * frametime_element_width);
+        frametime_r.w = frametime_element_width;
+        frametime_r.h = abs(percent);
+
+        if (frametime_r.h > graph_max_height) {
+          frametime_r.h = graph_max_height;
+        }
+
+        if (frametime_avg < frametime) {
+          frametime_r.y = frametime_r_base.y - frametime_r.h + 1;
+        } else {
+          frametime_r.y = frametime_r_base.y;
+        }
+                
+        mu_draw_rect(ctx, frametime_r, mu_color(config.key_color[0], config.key_color[1], config.key_color[2], config.key_color[3]));
+    }
+
+    mu_draw_rect(ctx, frametime_r_base, mu_color(config.key_color[0], config.key_color[1], config.key_color[2], config.key_color[3]));
+
     mu_end_window(ctx);
   }
 
   mu_end(ctx);
-
+  
   mu_Command *cmd = NULL;
   while (mu_next_command(ctx, &cmd)) {    
     switch (cmd->type) {
